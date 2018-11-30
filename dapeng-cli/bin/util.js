7126bb53157d36e9146919a8446c7f4d89fa8c44
dapeng <fengzq@vdaoyun.com>
2018-11-30 12:34:05 +0800

    v 0.0.5

--- dapeng-cli/bin/util.js
+++ dapeng-cli/bin/util.js
@@ -2,27 +2,31 @@
 let fs  = require('fs');
 let join = require('path').join;
 const handlebars = require('handlebars');
+const path = require('path');
+
+const exts = ['.png', '.jpg', '.mp3', '.mp4', '.jpeg', '.gif']
 
 function copyDirSyncHandlebar(resource, targetPath, meta) {
     if (!fs.existsSync(targetPath))  {
-        fs.mkdir(targetPath, function (error) {
-            console.log('    ',error)
-        });
+        fs.mkdirSync(targetPath);
     }
     let files = fs.readdirSync(resource);
     files.forEach((val, index) => {
         let fPath = join(resource, val);
         let stats = fs.statSync(fPath);
+        let ext = path.extname(val);
         let tPath = join(targetPath, val);
         if (stats.isFile()) {
             console.log('    ', `正在构建文件: ${tPath}`)
-            const content = fs.readFileSync(fPath).toString();
-            const result = handlebars.compile(content)(meta);
-            fs.writeFileSync(tPath, result);
+            if (exts.includes(ext)) {
+                fs.writeFileSync(tPath, fs.readFileSync(fPath));
+            } else {
+                const content = fs.readFileSync(fPath).toString();
+                const result = handlebars.compile(content)(meta);
+                fs.writeFileSync(tPath, result);
+            }
         } else {
-            fs.mkdir(tPath, function (error) {
-                console.log('    ',error)
-            });
+            fs.mkdirSync(tPath);
             copyDirSyncHandlebar(fPath, tPath, meta);
         }
     })
@@ -38,7 +42,7 @@ function copyDirSync(resource, targetPath) {
         let stats = fs.statSync(fPath);
         let tPath = join(targetPath, val);
         if (stats.isFile()) {
-            console.log(`\t正在构建文件: ${tPath}`)
+            console.log(`\t正在构建文件: ${tPath} ${val}`);
             fs.writeFileSync(tPath, fs.readFileSync(fPath));
         } else {
             fs.mkdir(tPath, function (error) {
