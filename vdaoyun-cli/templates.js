4d2119d9574ff152e5b8761162f46f8f526829b1
dapeng <fengzq@vdaoyun.com>
2018-12-12 11:17:41 +0800

    0.0.8 新增模板

--- vdaoyun-cli/templates.js
+++ vdaoyun-cli/templates.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 const list = [
     {
-        'name': 'vue-weixin',
-        'desc': '构建基于vue开发的微信前端项目',
+        'name': 'vue-weixin-vux',
+        'desc': '构建基于vue-vux-ui开发的微信前端项目',
         'path': 'github:vdapeng/vue-weixin-template#master',
+        'languages': 'vue'
+    },
+    {
+        'name': 'vue-weixin-mint',
+        'desc': '构建基于vue-mint-ui开发的微信前端项目',
+        'path': 'github:vdapeng/vue-weixin-template#mint-ui',
         'languages': 'vue'
     }
 ]
 
-const names = ['vue-weixin']
+const names = ['vue-weixin-vux', 'vue-weixin-mint']
 
 function getTemplate (name) {
     for (let key in this.list) {
